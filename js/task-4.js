2e7a5b3564cf6fd21144b9fac23e5507dc36e6cd
Sokirman <sokirman@gmail.com>
2024-06-04 22:09:16 +0300

    commit all

--- js/task-4.js
+++ js/task-4.js
@@ -3,11 +3,11 @@ function getShippingCost(country) {
     case "China":
       return "Shipping to ${country} will cost 100 credits";
     case "Chile":
-      return "Shipping to Chile will cost 250 credits";
+      return "Shipping to ${country} will cost 250 credits";
     case "Australia":
-      return "Shipping to Australia will cost 170 credits";
+      return "Shipping to ${country} will cost 170 credits";
     case "Jamaica":
-      return "Shipping to Jamaica will cost 120 credits";
+      return "Shipping to ${country} will cost 120 credits";
     default:
       return "Sorry, there is no delivery to your country";
   }
